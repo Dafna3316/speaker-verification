dbf99dd6e346357f749a29d8be16889bc4e877da
Jonathan Dafna <81yonatand4@dalet.school>
2026-05-21 19:56:38 +0300

    Update snippet to match Notebook

--- book/snippets/Notebook/04-dataset-import.py
+++ book/snippets/Notebook/04-dataset-import.py
@@ -1,3 +1,2 @@
 %aimport dataset
-from dataset import TripleAudioDataset, triple_ds, DualAudioDataset,
-  dual_ds, decode_one_file_nocache, read_and_crop_direct
+from dataset import TripleAudioDataset, triple_ds, DualAudioDataset, dual_ds
